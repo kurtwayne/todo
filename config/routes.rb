39b1aeda6c99aab5b64389b6077387169d1c1589
kurtwayne <kurtwgardner@gmail.com>
2016-07-12 22:25:22 -0600

    Added ability to create users from the command line

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,14 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   namespace :api, defaults: { format: :json } do
-    resources :users 
-  end
+    resources :users do
+      resources :lists
+    end
 
+    resources :lists, only: [] do
+      resources :items, only: [:create]
+    end
+
+    resources :items, only: [:destroy]
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708222219) do
+ActiveRecord::Schema.define(version: 20160712151953) do
 
   create_table "items", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
   create_table "lists", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "name"
+    t.string   "permissions"
   end
 
   create_table "users", force: :cascade do |t|
